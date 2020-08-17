f1e2e15add31dcf480464b67c4b745d2da091102
TheBrunno <brunosilvadeoliveira.10@gmail.com>
2020-08-16 22:41:36 -0300

    ataque

--- Module.py
+++ Module.py
@@ -2,13 +2,26 @@ from random import randint
 
 class Player:
     lvl = 1
-    adv_lvl = 0
     vida = lvl * 100
+    def __init__(self, nome):
+        self.nome_player = nome
+        self.exp_player = 0
+
     def ataque(self, arm, monster):
         monster.life -= arm.ataque
-        if monster.life < 0:
+        exp = 0
+        if monster.life <= 0:
             monster.life = 0
-        return monster.life
+            if monster.vivo:
+                monster.vivo = False
+                exp += monster.exp
+                return f'{self.nome_player} matou {monster.name}.\n{self.nome_player} ganhou {exp} de exp'
+            else:
+                return f'{monster.name} ja esta morto'
+        else:
+            return f'{self.nome_player} atacou {monster.name} que ficou com {monster.life} de vida'
+
+        
 
 
 class Mochila:
@@ -34,10 +47,16 @@ class Arco:
     pass
 
 class Orc:
+    vivo = True
+    exp = 30
+    name = 'Orc'
     life = 100
     ataque = 20
 
 class Minotauro:
+    vivo = True
+    exp = 40
+    name = 'Minotauro'
     life = 150
     ataque = 30
 
